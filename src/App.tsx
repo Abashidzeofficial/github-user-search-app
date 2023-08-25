349a806244da2cbdc5b5c90c6c55d45e65e1e0d7
Giorgi Abashidzee <abashidze.official@gmail.com>
2023-08-25 11:51:43 +0400

    newUpdeat

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,29 @@ import './component/styles.css';
 import Header from './component/Header';
 import moon from './images/icon-moon.svg';
 import sun from './images/icon-sun.svg';
+import searchIcon from './images/icon-search.svg';
 
 const App: React.FC = () => {
   return (
     <div className="wrapper">
-      <p className="header-txt">devfinder</p>
-      <div className='contrast-dark'>
-        <p className='dark'>DARK </p>
-        <img src={moon} alt="dark and light" className="dl" />
-      </div>
-      <div className="contrast-light">
-        <p className="light">LIGHT</p>
-        <img src={sun} alt="sun-light" className="di" />
+      <div className="header-group">
+        <p className="header-txt">devfinder</p>
+        {/* <div className='contrast-dark'>
+          <p className='dark'>DARK </p>
+          <img src={moon} alt="dark and light" className="dl" />
+        </div> */}
+        <div className="contrast-light">
+          <p className="light">LIGHT</p>
+          <img src={sun} alt="sun-light" className="sun-light" />
+        </div>
       </div>
       <div className="input-group">
         <div className="input-graph">
-          <input type="text" className="input-txt" />
+          <input type="text" className="input-txt" placeholder="Search GitHub users..." />
+
         </div>
         <button className="search-btn">Search</button>
+        <img src={searchIcon} alt="searchIcon" className="search-icon" />
       </div>
       <div className="container">
         <Header />
